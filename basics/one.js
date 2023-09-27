9220892e20cd85c6dc87d7e9a69876d44cedf5a7
Harshit Singh <114082380+Harshit7492@users.noreply.github.com>
2023-09-27 13:19:57 +0000

    container config done

--- basics/one.js
+++ basics/one.js
@@ -1,1 +1,16 @@
 console.log("Hello world")
+console.log("to save file for future use go on 4th icon open it click on files on which u want to add bt clicking + then write command on search bar container config done ok click on 3 dots click on push button")
+const accountid= 26732;
+let accname="harsh94621@gmai.com";
+var accpassw="8392";
+acccity="kanpur";//without variable declaration we ac print not used wrong way withot memory 
+let accstate;// show undefined bcoz no memory assigned 
+
+// accountid=656;  //not allowed const cannot be updated
+console.log(accountid)
+accname= "harshit123@gmail.com";
+accpassw="dk0430";
+acccity="banda";
+console.table([ accountid ,accname,accpassw,acccity,accstate])
+
+// var datatype have scope problem (also in javascript) if some have sma e var type name it changes all value 
